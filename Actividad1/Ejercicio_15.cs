f231bea107e12a788c44457eb1ac77a148996aa1
JuanSebastianRojasRivera <99339638+JuanSebastianRojasRivera@users.noreply.github.com>
2022-05-26 11:19:21 -0500

    Create Ejercicio_15.cs

---
+++ Actividad1/Ejercicio_15.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Ejercicio15
+{
+    class Metodos
+    {
+        static void Main(string[] args)
+        {
+            string Algo="Hello my name is Jaider Doncel, How are you?";
+
+            Console.WriteLine(Algo);
+            Console.WriteLine(Algo.Remove(21));
+            Console.WriteLine(Algo.Remove(5,16));
+        }
+    } 
+}
