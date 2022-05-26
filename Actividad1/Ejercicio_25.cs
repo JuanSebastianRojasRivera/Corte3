7657a639f8c0a5b1ec0d990a6005cb2fd53f4383
JuanSebastianRojasRivera <99339638+JuanSebastianRojasRivera@users.noreply.github.com>
2022-05-26 11:26:42 -0500

    Create Ejercicio_25.cs

---
+++ Actividad1/Ejercicio_25.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Text;
+
+class Ejercicio25
+{
+
+    public static void Main(String[] args)
+    {
+
+        StringBuilder abc
+          = new StringBuilder(" 15,30,45,60,75 ");
+
+
+        Console.WriteLine(" El string contiene los siguientes datos: "
+                            + abc.ToString());
+        Console.WriteLine(" Estos datos son verdaderos "); 
+    }
+}
