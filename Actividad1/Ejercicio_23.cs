ecc2f12f4b2ed1e1ae0ad73b9ab67f168b59a35d
JuanSebastianRojasRivera <99339638+JuanSebastianRojasRivera@users.noreply.github.com>
2022-05-26 11:25:34 -0500

    Create Ejercicio_23.cs

---
+++ Actividad1/Ejercicio_23.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+
+class Ejercicio23
+{
+
+    public static void Main(String[] args)
+    {
+
+        StringBuilder ABC =
+        new StringBuilder("Algo de algo");
+
+
+        Console.WriteLine("String contiene = "
+                          + ABC.ToString());
+    }
+}
