ba00f73c95884f63ccfe590e318ccf98077cdedc
JuanSebastianRojasRivera <99339638+JuanSebastianRojasRivera@users.noreply.github.com>
2022-05-26 11:02:25 -0500

    Create Ejercicio3.cs

---
+++ Actividad1/Ejercicio3.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections; 
+
+ 
+namespace Ejercicio3
+{
+  public class Ejercicio3
+  {
+    public static void Main()
+    {
+         String cadena1 = "Hello";
+         String cadena2 = "World";
+         String ca = String.Concat(cadena1, " " , cadena2);
+         Console.WriteLine(ca); 
+    }
+  }
+}
