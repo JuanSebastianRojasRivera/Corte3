c54e7640a8722d776f1e1a6d162b8d63c30f1c92
JuanSebastianRojasRivera <99339638+JuanSebastianRojasRivera@users.noreply.github.com>
2022-05-26 11:00:25 -0500

    Update and rename Ejercicio1 to Ejercicio1.cs

---
+++ Actividad1/Ejercicio1.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+
+
+namespace Ejercicio1
+{
+    public class Ejercicio1
+    {
+        public static void Main()
+        {
+            int numero = 450; 
+            String numero1 = numero.ToString();
+            System.Console.WriteLine(numero1);
+        }
+    }
+}
