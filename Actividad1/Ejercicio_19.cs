8e4c57e9fd83b42ab856eff9df7a9b84e66f2470
JuanSebastianRojasRivera <99339638+JuanSebastianRojasRivera@users.noreply.github.com>
2022-05-26 11:21:54 -0500

    Create Ejercicio_19.cs

---
+++ Actividad1/Ejercicio_19.cs
@@ -0,0 +1,23 @@
+using System;
+  
+class Ejercicio19 {
+      
+      
+    public static void Main()
+    {
+        string x1 = " ABCD";
+        string x2 = " ABCD ";
+  
+
+        Console.WriteLine("Antes");
+        Console.WriteLine(X1);
+        Console.WriteLine(X2);
+  
+        Console.WriteLine("");
+  
+
+        Console.WriteLine("Despues:");
+        Console.WriteLine(X1.Trim());
+        Console.WriteLine(X2.Trim());
+    }
+}
