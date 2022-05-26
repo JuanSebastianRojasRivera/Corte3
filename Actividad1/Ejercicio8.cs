c4ec48ee609d3ddf25ddafad5c2960d47c0701ac
JuanSebastianRojasRivera <99339638+JuanSebastianRojasRivera@users.noreply.github.com>
2022-05-26 11:08:27 -0500

    Create Ejercicio8.cs

---
+++ Actividad1/Ejercicio8.cs
@@ -0,0 +1,15 @@
+namespace Ejercicio8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string palabra = "este es el ejercicio de Endswith";
+           
+            Console.WriteLine(palabra);
+            Console.WriteLine(palabra.EndsWith("Endswith"));
+            Console.WriteLine(palabra.EndsWith("Ejercicio"));
+        }
+    }
+}
